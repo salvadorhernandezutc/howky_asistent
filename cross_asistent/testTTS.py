a3a9f53e3e638393db145ce2a3338a4ea0d1d0f6
chava-utc <salvadorh@utc.edu.mx>
2025-04-25 12:29:46 -0600

    add comments in TTS test

--- cross_asistent/testTTS.py
+++ cross_asistent/testTTS.py
@@ -9,10 +9,12 @@ openai = AsyncOpenAI()
 async def main() -> None:
     async with openai.audio.speech.with_streaming_response.create(
         model="gpt-4o-mini-tts",
+        # model="tts-1",
+        # model="tts-1-hd", # No Interpreta el español correctamente
         voice="ash",
         # voice="onyx",
         input="¡Hoy es un día maravilloso para construir algo que la gente ama!",
-        instructions="Habla en un tono neutro.",
+        instructions="Habla en un tono neutro.", # Es unicamente para gpt-4o-mini-tts
         response_format="pcm",
     ) as response:
         await LocalAudioPlayer().play(response)
